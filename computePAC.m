4a3683da604d86aab3bd622cac025269ce06be91
Roee Gilron <roee@RoeeG.local>
2018-02-08 21:16:03 -0800

    adding surrogates and options around that - thanks Doris

--- computePAC.m
+++ computePAC.m
@@ -34,13 +34,18 @@ function results = computePAC(data,sr,varargin)
 %  6. computeSurrogates - compute surrogates for statistical purposes
 %     example: computePAC(data,sr,'useparfor',0,'computeSurrogates',1)
 %     default: 0
-%  7. regionnames - charachter, regions names to be computed (only relevant if computing
+%  7. numsurrogate - number of surrogates (non-parametric shuffels) to compute for statistical purposes
+%     example: computePAC(data,sr,'computeSurrogates',1,'numsurrogate',100)
+%     default: 100
+%  7. alphause - alpha use for for statistical purposes
+%     default: 0.05
+%  8. regionnames - charachter, regions names to be computed (only relevant if computing
 %     between two regions
 %     example: computePAC(data,sr,'PhaseFreqVector',2:2:50,'regionnames',{'GPi','Motor Cortex'})
 %     default: {'a1','a2'};
-%  8. plotdata - if true (1 -default) plot data, if false, don't plot 
+%  9. plotdata - if true (1 -default) plot data, if false, don't plot
 %     example: computePAC(data,sr,'PhaseFreqVector',2:2:50,'plotdata',0)
-%     default: 1 (plot data) 
+%     default: 1 (plot data)
 %
 %  Also accpets structure format, example:
 %  params.PhaseFreqVector = 2:2:50;
@@ -51,7 +56,7 @@ function results = computePAC(data,sr,varargin)
 %  Outputs:
 %  structure -  results which contain input fields as well as:
 %  1. Comodulogram
-%  
+%
 %
 %  plot of PAC in either 1 / 4 subplots:
 %  if data is 1 x time points - compute PAC within
@@ -63,7 +68,7 @@ function results = computePAC(data,sr,varargin)
 %  4 plot PAC between area 1 (amp) and area 2 (phase)
 
 
-%% Define function inputs, data, sr requires default values 
+%% Define function inputs, data, sr requires default values
 % Parse input arguments
 p = inputParser;
 p.CaseSensitive = false;
@@ -76,6 +81,8 @@ addParameter(p,'PhaseFreq_BandWidth',4,validationFcn)
 addParameter(p,'AmpFreq_BandWidth',10,validationFcn)
 addParameter(p,'useparfor',0,validationFcn)
 addParameter(p,'computeSurrogates',0,validationFcn)
+addParameter(p,'numsurrogate',100,validationFcn)
+addParameter(p,'alphause',0.05,validationFcn)
 addParameter(p,'plotdata',1,validationFcn)
 
 validationFcn = @(x) validateattributes(x,{'cell'},{'nonempty'});
@@ -91,6 +98,8 @@ AmpFreq_BandWidth    = p.Results.AmpFreq_BandWidth;
 useparfor            = p.Results.useparfor; % if true, user parfor, requires parallel computing toolbox
 regionames           = p.Results.regionnames;
 computesurr          = p.Results.computeSurrogates;
+numsurrogate         = p.Results.numsurrogate;
+alphause             = p.Results.alphause;
 plotdata             = p.Results.plotdata;
 
 %% Load data
@@ -103,33 +112,40 @@ t             = (1:data_length)*dt;
 
 if plotdata
     hfig          = figure('Visible','off');
-    hfig.Position = [1000         666        1132         672];
+    %     hfig.Position = [1000         666        1132         672];
+end
+
+if useparfor 
+    if isunix & ~ismac
+        % requires parpool toolbox 
+        pool = parpool('roee',20); % this is specific to our servers; 
+    end
 end
 
 if data_size == 1
-    numplots = 1; 
+    numplots = 1;
 else
-    numplots = 4; 
+    numplots = 4;
 end
 for aa = 1:numplots
-    if numplots == 1 
-        datAmp = lfp; 
-        datPha = lfp; 
+    if numplots == 1
+        datAmp = lfp;
+        datPha = lfp;
         ttlAmp = '';
         ttlPha = '';
-    elseif numplots == 4 
-        switch aa 
-            case 1 
+    elseif numplots == 4
+        switch aa
+            case 1
                 datAmp = lfp(1,:);
                 datPha = lfp(1,:);
                 ttlAmp = sprintf('%s',regionames{1});
                 ttlPha = sprintf('%s',regionames{1});
-            case 2 
+            case 2
                 datAmp = lfp(2,:);
                 datPha = lfp(2,:);
                 ttlAmp = sprintf('%s',regionames{2});
                 ttlPha = sprintf('%s',regionames{2});
-            case 3 
+            case 3
                 datAmp = lfp(1,:);
                 datPha = lfp(2,:);
                 ttlAmp = sprintf('%s',regionames{1});
@@ -150,16 +166,17 @@ for aa = 1:numplots
     for ii=1:length(AmpFreqVector)
         Af1 = AmpFreqVector(ii);
         Af2 = Af1+AmpFreq_BandWidth;
-        AmpFreq=eegfilt_pac(datAmp,srate,Af1,Af2); % just filtering
+        AmpFreq = eegfilt_pac(datAmp,srate,Af1,Af2); % just filtering
         AmpFreqTransformed(ii, :) = abs(hilbert(AmpFreq)); % getting the amplitude envelope
     end
     
     for jj=1:length(PhaseFreqVector)
         Pf1 = PhaseFreqVector(jj);
         Pf2 = Pf1 + PhaseFreq_BandWidth;
-        PhaseFreq=eegfilt_pac(datPha,srate,Pf1,Pf2); % this is just filtering
+        PhaseFreq = eegfilt_pac(datPha,srate,Pf1,Pf2); % this is just filtering
         PhaseFreqTransformed(jj, :) = angle(hilbert(PhaseFreq)); % this is getting the phase time series
     end
+    
     
     %% Do comodulation calculation
     start   = tic;
@@ -172,8 +189,10 @@ for aa = 1:numplots
     end
     winsize = 2*pi/nbin;
     lognbin = log(nbin);
+    
     % using this indexing scheme allows for more efficient parfor
-    pairuse = [];cnt = 1;
+    pairuse = [];
+    cnt = 1;
     for jj=1:length(AmpFreqVector)
         for ii=1:length(PhaseFreqVector)
             puse1(cnt) = ii;
@@ -193,31 +212,71 @@ for aa = 1:numplots
             Comodulogram(p) = ModIndex_v3(PhaseFreqTransformed(puse1(p), :), AmpFreqTransformed(puse2(p), :)', position,nbin,winsize,lognbin);
         end
     end
-    Coreshaped = reshape(Comodulogram,length(PhaseFreqVector),length(AmpFreqVector));
-    %% XXX NEED TO IMPLEMENT 
-    %run surrogates (statistics) if desired
-%     if ~isempty(skip)
-%         for s=1:numsurrogate
-%             Amp_surr =[AmpFreqTransformed(jj,skip(s):end) AmpFreqTransformed(jj,1:skip(s)-1)];
-%             [MI_S,MeanAmp_S]=ModIndex_v2(PhaseFreqTransformed(ii,:), Amp_surr, position);
-%             MI_surr(s) = MI_S;
-%         end
-%         high_surr = length(find(abs(MI_surr)>=(abs(Comodulogram(counter1,1)))));
-%         calculate statistics
-%         pComodulogram(counter1,1) = high_surr./numsurrogate;
-%         zComodulogram(counter1,1) = (Comodulogram(counter1,1) - mean(abs(MI_surr)))./std(abs(MI_surr));
-%     end
     
+    Com_reshaped = reshape(Comodulogram,length(PhaseFreqVector),length(AmpFreqVector));
+    
+    %% initialize variables for surrogate
+    if computesurr
+        % choose # of points at least sr from start and end of recording 
+        rng(1); % to get same answer each time - be careful! 
+        skip = randperm(size(AmpFreqTransformed,2)-sr*2,numsurrogate)+sr; 
+        % get skip idxs (to avoid doing this for each shuffle 
+        for s = 1:numsurrogate
+            idxsurr(s,:) = [skip(s):size(AmpFreqTransformed,2) 1:skip(s)-1];
+        end
+        % run surrogates (statistics) if desired
+        for s=1:numsurrogate
+            if useparfor 
+                parfor p = 1:size(puse1,2)
+                    Comodulogram_surr(s,p) = ModIndex_v3(PhaseFreqTransformed(puse1(p),:),  AmpFreqTransformed(puse2(p), idxsurr(s,:))', position,nbin,winsize,lognbin);
+                end
+            else
+                for p = 1:size(puse1,2)
+                    Comodulogram_surr(s,p) = ModIndex_v3(PhaseFreqTransformed(puse1(p),:),  AmpFreqTransformed(puse2(p), idxsurr(s,:))', position,nbin,winsize,lognbin);
+                end
+            end
+        end
+        % compute z score 
+        MI_surr = Comodulogram_surr;
+        [surrogate_mean,surrogate_std] = normfit(MI_surr); % fit gaussian to surrogate data, uses normfit.m from MATLAB Statistics toolbox
+        zComodulogram = (abs(Comodulogram) - surrogate_mean )./ surrogate_std;
+        
+        % compute p values 
+        compMatrix = repmat(Comodulogram,numsurrogate,1);
+        pvals = 1 - sum(abs(compMatrix) >= abs(MI_surr),1)./numsurrogate; % two sided p-value
+        % set any Pval that is zero the effective max pval
+        pvals(pvals==0) = 1/numsurrogate;
+        [pvalssig,critp] = fdr_bh(pvals,alphause);
+        % give value of zero to pvalues that are not significant 
+        Comodulogram_surr = Comodulogram;
+        Comodulogram_surr(~pvalssig) = 0;
+
+     
+        % reshape outputs 
+        pvals_reshape= reshape(pvals,length(PhaseFreqVector),length(AmpFreqVector));
+        Com_surr_reshaped= reshape(Comodulogram_surr,length(PhaseFreqVector),length(AmpFreqVector));
+        zCom_reshaped = reshape(zComodulogram,length(PhaseFreqVector),length(AmpFreqVector));
+    else
+        Com_surr_reshaped = [];
+        zCom_reshaped = [];
+        pvals = []; 
+    end
     %%
     fprintf('comod calc done in %f secs \n',toc(start));
-    % save data: 
-    results(aa).Comodulogram        = Coreshaped; 
+    % save data:
+    results(aa).zComodulogram       = zCom_reshaped; % this will by empty if you didn't run surrogates 
+    results(aa).Comodulogram_surr   = Com_surr_reshaped;% this will by empty if you didn't run surrogates 
+    results(aa).pvals               = pvals_reshape;% this will by empty if you didn't run surrogates 
+    results(aa).Comodulogram        = Com_reshaped;
     results(aa).PhaseArea           = ttlPha;
     results(aa).ttlAmp              = ttlAmp;
     results(aa).AmpFreqVector       = AmpFreqVector;
     results(aa).PhaseFreqVector     = PhaseFreqVector;
     results(aa).PhaseFreq_BandWidth = PhaseFreq_BandWidth;
     results(aa).AmpFreq_BandWidth   = AmpFreq_BandWidth;
+    results(aa).computesurr         = computesurr;
+    results(aa).numsurrogate        = numsurrogate;
+    results(aa).alphause            = alphause;
     
     %% plotting
     if plotdata
@@ -231,14 +290,16 @@ for aa = 1:numplots
         else
             ttlgrp = 'PAC within';
         end
-        contourf(PhaseFreqVector+PhaseFreq_BandWidth/2,AmpFreqVector+AmpFreq_BandWidth/2,Coreshaped',30,'lines','none')
+        
+        
+        contourf(PhaseFreqVector+PhaseFreq_BandWidth/2,AmpFreqVector+AmpFreq_BandWidth/2,Com_reshaped',30,'lines','none')
         set(gca,'fontsize',14)
         ttly = sprintf('Amplitude Frequency %s (Hz)',ttlAmp);
         ylabel(ttly)
         ttlx = sprintf('Phase Frequency %s (Hz)',ttlPha);
         xlabel(ttlx)
         title(ttlgrp);
-        colorbar
+        %colorbar
     end
 end
 if plotdata
