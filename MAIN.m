22eb05a5b95aca2d1d383099c42bf4048880f498
Roee Gilron <roee@RoeeG.local>
2018-02-08 21:19:30 -0800

    adding surrogates and options around that - thanks Doris

--- MAIN.m
+++ MAIN.m
@@ -2,17 +2,17 @@ function MAIN()
 %% This is a caller routine to evaluate the user of the parfor on our server 
 % the purpose is to test parfor performance with a large number of
 % surrogates on our server. 
-load ExtractHGHFOOpenField.mat; 
+load('ExtractHGHFOOpenField.mat'); 
 % set paramaters: 
 params.PhaseFreqVector      = 2:2:50;
 params.AmpFreqVector        = 100:5:200;
 params.PhaseFreq_BandWidth  = 4;
 params.AmpFreq_BandWidth    = 10;
-params.computesurr          = 1;
+params.computeSurrogates    = 1;
 params.numsurrogate         = 100;
 params.alphause             = 0.05;
 params.plotdata             = 1;
 params.useparfor            = 1; % if true, user parfor, requires parallel computing toolbox
 
-computePAC(lfpHFO,srate,params);
+computePAC(lfpHFO,1000,params);
 end
